66effe8bbc87764615f0510c124a22e5c105e966
Weronika <weronika.kowalska@student.put.poznan.pl>
2020-05-27 18:27:47 +0200

    sending and receiving images as bytes

--- server/env/jigSolve/routes.py
+++ server/env/jigSolve/routes.py
@@ -1,17 +1,29 @@
-from flask import request, make_response
+from flask import request, make_response, send_file
 from jigSolve import app, api_logic
+import requests
 
+
+SOLVED_PUZZLES_PATH = 'puzzles_picture.png' 
 
 @app.route("/")
 def home():
     return "Welcome!"
 
 
-@app.route("/puzzle", methods=['POST'])
+@app.route("/upload-puzzle", methods=['POST'])
 def post_one_puzzle():
+    one_puzzle = request.files['puzzle_piece_picture']
+    filename = 'jeden_puzzel.png'
+    print(one_puzzle)
+    one_puzzle.save(filename)
+    print('One puzzle piece image saved')
     return "You send one puzzle!"
 
 
-@app.route("/puzzles", methods=['POST'])
+@app.route("/upload-puzzles", methods=['POST'])
 def post_entire_puzzles():
-    return "Here comes your pic!"
+    entire_puzzles = request.files['entire_puzzles_picture']
+    filename = 'cale_puzzle.png'
+    entire_puzzles.save(filename)
+    print('Entire puzzles image saved')
+    return "You send entire puzzles!"
---
+++ server/env/jigSolve/test_client.py
@@ -0,0 +1,24 @@
+import requests
+from io import StringIO
+import base64
+
+
+URL = 'http://127.0.0.1:80/'
+
+
+def post_one_puzzle():
+    endpoint = '/upload-puzzle'
+    data = {'puzzle_piece_picture': open('puzzle_piece.png', 'rb')}
+    response = requests.post(URL+endpoint, files=data)
+    print(response.content)
+
+def post_entire_puzzles():
+    endpoint = '/upload-puzzles'
+    data = {'entire_puzzles_picture': open('puzzles_picture.png', 'rb')}
+    response = requests.post(URL+endpoint, files=data)
+    print(response.content)
+
+
+if __name__ == '__main__':
+    post_one_puzzle()
+    post_entire_puzzles()
